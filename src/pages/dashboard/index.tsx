9e04fa06f60724d5df98857aec0735461fef4d45
lvelo <lveliz60@gmail.com>
2025-07-17 14:31:57 -0300

    Pagina dashboard

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Bienvenido al ERP</h1>
+      <p>Este es tu panel principal. Pronto tendrás menús para Inventarios, Ventas, Compras, etc.</p>
+    </div>
+  );
+}

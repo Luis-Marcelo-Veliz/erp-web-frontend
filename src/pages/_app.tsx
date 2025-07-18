5ebd9fa778ae65c0d860cf2ca3dbaabc5b6daf73
lvelo <lveliz60@gmail.com>
2025-07-18 00:21:48 -0300

    proteger rutas con fetch y JWT

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,3 +9,18 @@ function MyApp({ Component, pageProps }) {
   }, [router]);
   return <Component {...pageProps} />;
 }
+export default MyApp;
+
+async function getProtectedData() {
+  const token = sessionStorage.getItem('token');
+  const response = await fetch('http://localhost:3000/protected', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) throw new Error('Acceso no autorizado');
+  const data = await response.json();
+  console.log(data);
+}

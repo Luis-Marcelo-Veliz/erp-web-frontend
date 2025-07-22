a59edf01d6d3ab3a155342fac472bf5e377923fe
lvelo <lveliz60@gmail.com>
2025-07-22 09:29:56 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,13 @@ import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-useEffect(() => {
+/*useEffect(() => {
   const token = sessionStorage.getItem('token');
   if (!token && router.pathname !== '/login') {
     // mejor replace para no agregar al historial
     router.replace('/login');
   }
-}, [router]);
+}, [router]);*/
   return <Component {...pageProps} />;
 }
 export default MyApp;

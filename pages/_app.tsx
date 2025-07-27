b92029e430a01154f5408e6f2bb0573b2990e399
lvelo <lveliz60@gmail.com>
2025-07-26 23:51:45 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,19 @@
+// pages/_app.tsx
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { getBackendBaseUrl } from '../utils/api';  // tu helper
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-/*useEffect(() => {
-  const token = sessionStorage.getItem('token');
-  if (!token && router.pathname !== '/login') {
-    // mejor replace para no agregar al historial
-    router.replace('/login');
-  }
-}, [router]);*/
+
+  useEffect(() => {
+    const token = sessionStorage.getItem('token');
+    if (!token && router.pathname !== '/login') {
+      router.replace('/login');
+    }
+  }, [router]);
+
   return <Component {...pageProps} />;
 }
-export default MyApp;
 
-async function getProtectedData() {
-  const token = sessionStorage.getItem('token');
-  const response = await fetch('http://localhost:3000/protected', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
-  });
-  if (!response.ok) throw new Error('Acceso no autorizado');
-  const data = await response.json();
-  console.log(data);
-}
+export default MyApp;

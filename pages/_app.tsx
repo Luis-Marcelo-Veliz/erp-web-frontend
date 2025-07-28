85868d4d16dd6d6369d17c73ca73bb83e708e7f3
lvelo <lveliz60@gmail.com>
2025-07-28 14:13:16 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
-// pages/_app.tsx
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import { getBackendBaseUrl } from '../utils/api';  // tu helper
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
+const router = useRouter();
 
-  useEffect(() => {
-    const token = sessionStorage.getItem('token');
-    if (!token && router.pathname !== '/login') {
-      router.replace('/login');
-    }
-  }, [router]);
+useEffect(() => {
+const token = sessionStorage.getItem('token');
+if (!token && router.pathname !== '/login') {
+router.replace('/login');
+}
+}, \[router]);
 
-  return <Component {...pageProps} />;
+return \<Component {...pageProps} />;
 }
 
 export default MyApp;

867f67b7bf64a20689e236c26bd2765c40cd29b0
lvelo <lveliz60@gmail.com>
2025-07-28 14:35:40 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-/\*\* @type {import('next').NextConfig} \*/
-const dev = process.env.NODE\_ENV !== 'production';
+/** @type {import('next').NextConfig} */
+const dev = process.env.NODE_ENV !== 'production';
 
 const nextConfig = {
-async rewrites() {
-return \[
-{
-source: '/api/\:path\*',
-destination: dev
-? process.env.NEXT\_PUBLIC\_BACKEND\_URL + '/api/\:path\*'
-: process.env.NEXT\_PUBLIC\_API\_URL + '/api/\:path\*',
-},
-];
-},
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: dev
+          ? process.env.NEXT_PUBLIC_BACKEND_URL + '/api/:path*'
+          : process.env.NEXT_PUBLIC_API_URL + '/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
